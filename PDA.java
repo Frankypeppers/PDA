f0e019ae7e3998e220770edfba2c0ea8744f1690
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-11-09 11:43:10 -0800

    finished(?)

--- PDA.java
+++ PDA.java
@@ -24,22 +24,28 @@ public class PDA
         boolean ShouldContinue = true;
         while(ShouldContinue == true) {
             int age = -1;
-            int LOWER_BOUND = 0;
-            int UPPER_BOUND = 0;
-            LOWER_BOUND = age/2 + 7;
-            UPPER_BOUND = age*2 + 7;
             System.out.println("How old are you?");
             System.out.println("Enter 0 to quit");
             try {
                 age = scanner.nextInt();
                 if (age == 0) {
                     ShouldContinue = false;
-                } else if (age < LOWER_BOUND) {
-                    System.out.println(age + " is too young!!");
+                } else if(age == -1) {
+                    System.out.println("");
                 } else {
-                    LOWER_BOUND = age/2 + 7;
-                    UPPER_BOUND = (age - 7)*2;
-                    System.out.println("You can date between " + LOWER_BOUND + " and " + UPPER_BOUND);
+                    int LOWER_BOUND = 0;
+                    int UPPER_BOUND = 0;
+                    LOWER_BOUND = (age/2) + 7;
+                    UPPER_BOUND = (age - 7) * 2;
+                    if (age < LOWER_BOUND) {
+                        System.out.println(age + " is too young!!");
+                    } else if (age > UPPER_BOUND) {
+                        System.out.println(age + " is too young!!");
+                    } else {
+                        LOWER_BOUND = age/2 + 7;
+                        UPPER_BOUND = (age - 7)*2;
+                        System.out.println("You can date between " + LOWER_BOUND + " and " + UPPER_BOUND);
+                    }
                 }
             } catch (InputMismatchException error) {
                 System.out.println("Please enter an integer");
